025a82b4667f90062cef629d5768e607d7329db8
TomSawyer2 <6272867@qq.com>
2021-10-23 16:33:57 +0800

    chore：依赖处理

--- src/plugins/echarts.js
+++ src/plugins/echarts.js
@@ -6,11 +6,11 @@ let echarts = require('echarts/lib/echarts');
 // 引入折线图等组件
 require('echarts/lib/chart/line')
 require('echarts/lib/chart/pie')
-require('echarts/lib/chart/bar')
-require('echarts/lib/chart/radar')
+// require('echarts/lib/chart/bar')
+// require('echarts/lib/chart/radar')
 
 // // 引入提示框和title组件，图例
-// require('echarts/lib/component/tooltip')
+require('echarts/lib/component/tooltip')
 require('echarts/lib/component/grid')
 
 require('echarts/lib/component/title')
